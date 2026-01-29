a7c906eb3092864b81066e116b91836816d9b3a5
Tithe2001 <afrinakthertithebhuiyan@gmail.com>
2026-01-29 12:32:31 +0600

    layout

---
+++ src/layouts/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nav>
+        
+        <router-link to="/">Dashboard</router-link>
+        <router-link to="/role">Roles</router-link>
+        <router-link to="/role/create">Create Role</router-link>
+
+    </nav>
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
